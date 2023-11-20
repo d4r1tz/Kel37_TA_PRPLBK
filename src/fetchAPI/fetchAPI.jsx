59c7f559fc57e296f41960c8612ec2adba71415d
Daniel Ritz <d4r1tz@gmail.com>
2023-11-20 17:29:25 +0700

    Finished

--- src/fetchAPI/fetchAPI.jsx
+++ src/fetchAPI/fetchAPI.jsx
@@ -12,6 +12,10 @@ const OnePieceSeasonsProvider = ({ children, APIUrl, seasonIdCheck }) => {
         method: 'GET',
         url: `${APIUrl}`,
         headers: {
+          //List substitute keys if expired
+          //e84f1362b2mshfefe134ef9d5ebcp1ad6e0jsn45ddfdccca18
+          //96a118adf2msh586368af76efeb6p1d74ddjsn3f9e3061cf3d
+          //84901b8608mshe4b8b98c2c78c6ep119b84jsne99d30a5978d
           'X-RapidAPI-Key': '4beb7b230fmshbe9b190d607e724p1c5f8bjsn42956d8ee953',
           'X-RapidAPI-Host': 'one-piece-episodes.p.rapidapi.com',
         },
